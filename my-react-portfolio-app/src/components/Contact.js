9a349069815cbbdd59102721776377560efec14d
Domenic Wilhelm <Domenicsw92@gmail.com>
2021-07-09 15:22:17 -0600

    react

---
+++ my-react-portfolio-app/src/components/Contact.js
@@ -0,0 +1,18 @@
+export default function Contact() {
+    <section class="row justify-content-center mtb navimage">
+    <div class="col-12 col-sm-6 col-md-4">
+        <address>
+            <h2>Get in Touch</h2>
+
+            <h5>
+                Call @ <a href="tel:830.422.4541"></a>830.422.4541</a>
+            </h5>
+
+            <h5>
+                Email me at <a href="Domenicsw92@gmail.com">Domenicsw92@gmail.com</a>
+            </h5>
+
+        </address>
+    </div>
+    </section >
+}
---
+++ my-react-portfolio-app/src/components/Skills.js
@@ -0,0 +1,61 @@
+export default function Skills() {
+    <section class="single-section single-section alt-bg" id="">
+        <div class="container ">
+            <div class="row ">
+                <div class="col-12 ">
+                    <div class="section-heading ">
+                        <h2 class="section-title mb-5">Skills</h2>
+                        <div class="iconBox">
+                            <div class="">
+                                <images src="images/icons/html.png" alt="HTML" title="HTML">
+              </div>
+                                <div class="">
+                                    <images src="images/icons/css.png" alt="CSS" title="CSS">
+              </div>
+                                    <div class="">
+                                        <images src="images/icons/js.png" alt="JavaScript" title="JavaScript">
+              </div>
+                                        <div class="">
+                                            <images src="images/icons/jQuery.png" alt="jQuery" title="jQuery">
+              </div>
+                                            <div class="">
+                                                <images src="images/icons/bootstrap.png" alt="Bootstrap" title="Bootstrap">
+              </div>
+                                                <div class="">
+                                                    <images src="images/icons/NodeJS.png" alt="NodeJS" title="NodeJS">
+              </div>
+                                                    <div class="">
+                                                        <images src="images/icons/express.png" alt="Express" title="Express">
+              </div>
+
+
+
+                                                        <div class="">
+                                                            <images src="images/icons/MySQL.jpg" alt="MySQL" title="MySQL">
+              </div>
+                                                            <div class="">
+                                                                <images src="images/icons/mongodb.png" alt="Mongodb" title="Mongodb">
+              </div>
+                                                                <div class="">
+                                                                    <images src="images/icons/React.png" alt="React.js" title="React.js">
+              </div>
+                                                                    <div class="">
+                                                                        <images src="images/icons/mern.jpg" alt="MERN" title="MERN">
+              </div>
+                                                                        <div class="">
+                                                                            <images src="images/icons/npm_logo.png" alt="npm" title="npm">
+              </div>
+                                                                            <div class="">
+                                                                                <images src="images/icons/github-logo.png" alt="Github" title="Github">
+              </div>
+                                                                                <div class="">
+                                                                                    <images src="images/icons/heroku_icon_130912 (1).png" alt="Heroku" title="Heroku">
+              </div>
+            </div>
+          </div>
+                                                                                </div>
+                                                                            </div>
+
+                                                                        </div>
+  </section>
+}
