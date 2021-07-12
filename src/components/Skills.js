a1c8df9e537a460adb0366c23f672ab9ade5961a
Domenic Wilhelm <Domenicsw92@gmail.com>
2021-07-11 20:43:38 -0600

    added projects and skills

---
+++ src/components/Skills.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+function Skills() {
+    return (
+        <>
+        <section id="skills" class="container justify-content-center mtb">
+            <div class="row justify-content-center">
+                <h2 class="skills">Skills</h2>
+            </div>
+            <div class="row justify-content-center">
+
+                <div class="col-12 col-md-3 mx">
+                    <img src="images/icons/html.png" alt=""></img>
+                </div>
+                <div class="col-12 col-md-3 mx">
+                    <img src="images/icons/css.png" alt=""></img>
+                </div>
+                <div class="col-12 col-md-3 mx">
+                    <img src="images/icons/js.png" alt=""></img>
+                </div>
+            </div>
+            <div class="row justify-content-center ">
+                <div class="col-12 col-md-4 mx-5">
+                    <img src="images/icons/bootstrap.png" height="200" width="200" alt=""></img>
+                </div>
+                <div class="col-12 col-md-4 mx-5">
+                    <img src="images/icons/materialize.png" height="200" width="200" alt=""></img>
+                </div>
+                <div class="col-12 col-md-4 mx-5">
+                    <img src="images/icons/NodeJS.png" height="200" width="200" alt=""></img>
+                </div>
+                <div class="col-12 col-md-4 mx-5">
+                    <img src="images/icons/mysql.png" height="200" width="200" alt=""></img>
+                </div>
+            </div>
+            <div class="row justify-content-center ">
+                <div class="col-12 col-md-4 mx-5">
+                    <img src="images/icons/express.png" height="200" width="200" alt=""></img>
+                </div>
+                <div class="col-12 col-md-4 mx-5">
+                    <img src="images/icons/github-logo.png" height="200" width="200" alt=""></img>
+                </div>
+                <div class="col-12 col-md-4 mx-5">
+                    <img src="images/icons/heroku.png" height="200" width="200" alt=""></img>
+                </div>
+                <div class="col-12 col-md-4 mx-5">
+                    <img src="images/icons/React.png" height="200" width="200" alt=""></img>
+                </div>
+            </div>
+            <div class="row justify-content-center ">
+                <div class="col-12 col-md-4 mx-5">
+                    <img src="images/icons/express.png" height="200" width="200" alt=""></img>
+                </div>
+                <div class="col-12 col-md-4 mx-5">
+                    <img src="images/icons/github-logo.png" height="200" width="200" alt=""></img>
+                </div>
+                <div class="col-12 col-md-4 mx-5">
+                    <img src="images/icons/heroku.png" height="200" width="200" alt=""></img>
+                </div>
+                <div class="col-12 col-md-4 mx-5">
+                    <img src="images/icons/React.png" height="200" width="200" alt=""></img>
+                </div>
+            </div>
+            <div class="row justify-content-center ">
+                <div class="col-12 col-md-4 mx-5">
+                    <img src="images/icons/JQuery.png" height="200" width="200" alt=""></img>
+                </div>
+                <div class="col-12 col-md-4 mx-5">
+                    <img src="images/icons/mongodb.png" height="200" width="200" alt=""></img>
+                </div>
+                <div class="col-12 col-md-4 mx-5">
+                    <img src="images/icons/mern.jpg" height="200" width="200" alt=""></img>
+                </div>
+            </div>
+        </section>
+
+        </>
+    );
+}
+
+export default Skills;
